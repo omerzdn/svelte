7753dd462df606a57dda09af58abc199fee40c67
omer <96634886+omerzdn@users.noreply.github.com>
2022-11-23 00:27:55 +0300

    Update about.svelte

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -26,9 +26,7 @@
 	<pre>npm init svelte@next</pre>
 
 	<p>
-		The page you're looking at is purely static HTML, with no client-side interactivity needed.
-		Because of that, we don't need to load any JavaScript. Try viewing the page's source, or opening
-		the devtools network panel and reloading.
+		<h1>OMER</h1>
 	</p>
 </div>
 
